693e0c258759c1d82dd9cfb1c80594961b216d96
roxin <roxinbj@gmail.com>
2018-08-30 21:53:08 +0200

    new feature: read srtring from file or folder

--- exec/main.cpp
+++ exec/main.cpp
@@ -4,9 +4,19 @@
 #include <lib_dataImporter.hpp>
 #include <stdio.h>
 
-using namespace cv;
+
 int main(int argc, char** argv )
 {
+	Fw::FileReader fileReader{"../imagesPath.txt"};
+	std::vector<std::string> myVector = fileReader.getContent();
+    Fw::FolderReader folderReader{myVector[0]};
+
+	for(auto item : folderReader.getContent())
+	{
+		std::cout << "Zeile: " << item << "\n";
+	}
+
+	using namespace cv;
     if ( argc != 2 )
     {
         printf("usage: DisplayImage.out <Image_Path>\n");
--- libsrc/lib_dataImporter.hpp
+++ libsrc/lib_dataImporter.hpp
@@ -3,64 +3,122 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <dirent.h>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <sys/stat.h>
 
-namespace Fw
-{
+namespace Fw {
 
+class ImagesKeeper {
+public:
+	ImagesKeeper(const std::string& pathToImages) {
+	}
+	;
+	ImagesKeeper() = delete;
 
-class ImagesKeeper
-{
-   public:
-   ImagesKeeper(const std::string& pathToImages);
-   ImagesKeeper() = delete;
-   
-   private:
-      
-   std::vector<cv::Mat> m_images;   
-        
+private:
+
+	std::vector<cv::Mat> m_images;
+
 };
 
+class FileReader {
 
-std::vector<std::string> extractJpgsFromPath (const std::string& folderPath)
-{
-	std::vector<std::string> listOfImagePaths;
-	
-	DIR *dir;
-    struct dirent *ent;
-	if ((dir = opendir ("c:\\src\\")) != NULL) 
-	{
-	  /* print all the files and directories within directory */
-	  while ((ent = readdir (dir)) != NULL) 
-	  {
-    	printf ("%s\n", ent->d_name);
-	  }
-  	  closedir (dir);
-	} 
-	else 
-	{
- 		/* could not open directory */
-  		perror ("");
-  		return EXIT_FAILURE;
+public:
+	explicit FileReader(const std::string& pathToFile) :
+			m_pathToFile(pathToFile) {
+		openFile();
+		readContent();
 	}
-	
-	
+	~FileReader() {
+		closeFile();
+	}
 
+	const std::vector<std::string> getContent() {
+		return m_content;
+	}
 
+private:
+	void readContent() {
+		std::string line;
+		while (std::getline(m_file, line)) {
+			m_content.push_back(line);
+		}
+	}
+	void openFile() {
+		if (doesFileExist(m_pathToFile)) {
+			m_file.open(m_pathToFile);
+		}
+	}
+	void closeFile() {
+		m_file.close();
+	}
+	bool doesFileExist(const std::string& name) {
+		struct stat buffer;
+		return (stat(name.c_str(), &buffer) == 0);
+	}
+	std::ifstream m_file;
+	const std::string m_pathToFile;
+	std::vector<std::string> m_content;
+};
+
+class FolderReader {
+public:
+	FolderReader(const std::string& pathToFolder) {
+		if (openFolder(pathToFolder)) {
+			readFolder();
+		}
+	}
+
+	~FolderReader() {
+		closeFolder();
+	}
+	const std::vector<std::string> getContent() {
+		return m_filesInFolder;
+	}
+private:
+	bool openFolder(const std::string& pathToFolder) {
+		m_directory = opendir(pathToFolder.c_str());
+		if (m_directory == nullptr) {
+			std::cout << "Directory " << m_directoryPath <<" not found \n";
+			exit(1);
+			return false;
+		}
+		return true;
+	}
+
+	void readFolder() {
+		while ((m_ent = readdir(m_directory))) {
+			if (m_ent == nullptr) {
+				std::cout << "pent Problem";
+				exit(2);
+			}
+			// found true image (eg. no hidden files)
+			if (m_ent->d_namlen > 9) {
+				m_filesInFolder.push_back(m_ent->d_name);
+			}
+		}
+
+	}
+
+	void closeFolder() {
+		closedir(m_directory);
+	}
+
+	DIR* m_directory { nullptr };
+	struct dirent* m_ent { nullptr };
+	std::vector<std::string> m_filesInFolder;
+};
+
+//std::vector<cv::Mat>
+void fillImageVectorFromPath(const std::string& path) {
+//   using namespace cv;
+//
+//   Mat image;
+//
+//   image = imread(path);
 
 }
-
-
-std::vector<cv::Mat> fillImageVectorFromPath( const std::string& path)
-{
-   using namespace cv;
-   
-   Mat image;
-   
-   image = imread(path);
-
-}
-
-
-
 
 } // End of Namespace Fw
